5a3a3d1abc86f83e82d1cf8a5dc3a5523b871a7d
Tomsod M <tomsod-m@ya.ru>
2021-08-22 06:24:32 +0300

    Remove some duplicate MMExtension tables
    Before this commit, I had to ship a couple of tables (race stats and
    spell data) that duplicated the DLL patches to the memory.  They could
    be easily re-generated from memory, but since pre-existing tables take
    higher priority than my DLL, any tables leftover from a vanilla game
    would break my mod unless I provided my own.  A more elegant solution
    would be to delete these tables pre-emptively, but ZIP archives cannot
    do that.  Instead, this commit adds a small script that manually deletes
    any tables that start with a dummy "deleteme" line.  Sure, I still have
    to provide the dummy files, but it's better in that the DLL is now the
    sole source of modified data and I don't need to keep the tables
    updated.  The tables that generate *.bin files were left be, as I
    actually edit those files through them, so they're source code of sorts.

--- misc/ElementalFixes.lua
+++ misc/ElementalFixes.lua
@@ -9,3 +9,22 @@ offsets.CurrentEvtLines = mem.u4[0x446904]
 -- MMExtension is a bit more procative with it; without telling it
 -- the new address, the greetings won't be displayed at all.
 rawset(Game.NPCGreet, "?ptr", mem.u4[0x476e3a] - 8)
+
+-- The mod overwrites some exe data that is exposed by MMExtension tables.
+-- Since pre-existing tables would take higher precedence than elemental.dll,
+-- and outright deleting them is impossible as the mod is distributed
+-- via an archive, this simple hack allows overwriting them with dummies
+-- that will be deleted by the below code.  Sure, we could also distribute
+-- actual tables with mod-appropriate data, but that would mean duplicating
+-- the data, which is generally bad.
+for table in path.find("Data/Tables/*.txt") do
+    file = io.open(table)
+    if file then
+        line = file:read()
+        file:close()
+        if line == "deleteme" then
+            path.DeleteFile(table)
+        end
+    end
+end
+

Tomsod/elemental

commit 5bc3798a2de78b6c2f63a4d3c8fe12c511fb6eed

Various slaying weapon tweaks
Firstly, this commit nerfes Undead Slaying, which now only adds +50%
of weapon's damage.  2.0 made it too OP for how easily it can be
obtained.  Also two bug fixes: David now works on bows, and expired
temporary enchants are correctly removed before an attack.

elemental.c
@@ -596,6 +596,8 @@ static funcptr_t add_buff = (funcptr_t) 0x458519;
 static funcptr_t elem_damage = (funcptr_t) 0x439e16;
 static funcptr_t monster_resists = (funcptr_t) 0x427522;
 static funcptr_t monster_in_group = (funcptr_t) 0x438bce;
+static void __thiscall (*expire_temp_bonus)(struct item *item, long time)
+    = (funcptr_t) 0x458299;
 static funcptr_t is_artifact = (funcptr_t) 0x456d98;
 static int __thiscall (*get_skill)(void *player, int skill)
     = (funcptr_t) 0x48f87a;
@@ -1689,7 +1691,23 @@ static void __declspec(naked) temp_swiftness(void)
       }
 }
 
+// Make sure there are no lingering temp enchants
+// before checking for undead slaying / spectral / etc.
+static void __declspec(naked) expire_weapon(void)
+{
+    asm
+      {
+        lea ebx, [edi+0x1f0+eax*4]
+        mov ecx, ebx
+        push dword ptr [0xacce68] ; current time, high dword
+        push dword ptr [0xacce64] ; current time, low dword
+        call dword ptr ds:expire_temp_bonus
+        ret
+      }
+}
+
 // Store temp enchant in eax for the next chunk.
+// Also here: fix "of David" not being checked for.
 static void __declspec(naked) temp_bane_bow_1(void)
 {
     asm
@@ -1701,34 +1719,49 @@ static void __declspec(naked) temp_bane_bow_1(void)
         mov eax, dword ptr [ebx+8]
         no_temp:
         mov ebx, dword ptr [ebx+12]
+        cmp ebx, SPC_DAVID
+        jne not_david
+        mov edx, MG_TITAN
+        not_david:
         cmp ebx, SPC_UNDEAD_SLAYING
         ret
       }
 }
 
 // Check monster bane twice for both possible enchants.
+// Also lower Undead Slaying extra damage to 150%.
 static void __declspec(naked) temp_bane_bow_2(void)
 {
     asm
       {
-        mov ebx, eax
+        mov edi, eax
+        cmp edx, MG_UNDEAD
+        sete bl
         call dword ptr ds:monster_in_group
-        cmp ebx, SPC_UNDEAD_SLAYING
+        and ebx, eax
+        cmp edi, SPC_UNDEAD_SLAYING
         je undead
-        cmp ebx, SPC_DRAGON_SLAYING
-        je dragon
-        ret
-        dragon:
+        cmp edi, SPC_DRAGON_SLAYING
+        jne quit
         mov edx, MG_DRAGON
         jmp temp
         undead:
         mov edx, MG_UNDEAD
+        test eax, eax
+        setz bh
         temp:
         mov ecx, dword ptr [ebp+8]
         push eax
         call dword ptr ds:monster_in_group
         pop ecx
+        and bh, al
         or eax, ecx
+        quit:
+        mov ecx, esi
+        test ebx, ebx
+        jz not_undead
+        shr ecx, 1
+        not_undead:
         ret
       }
 }
@@ -1751,10 +1784,12 @@ static void __declspec(naked) temp_bane_melee_1(void)
 
 // Check bane twice for melee weapons.  Also fixes Gibbet.
 // We also check for backstab damage here.
+// Also, ensures that Undead Slaying only adds +50% damage.
 static void __declspec(naked) temp_bane_melee_2(void)
 {
     asm
       {
+        push 0 ; undead flag
         cmp ebp, CORSAIR
         je backstab
         cmp ebp, OLD_NICK
@@ -1768,7 +1803,12 @@ static void __declspec(naked) temp_bane_melee_2(void)
         mov eax, 1 ; return true
         jnz quit
         no_backstab:
+        cmp edx, MG_UNDEAD
+        jne not_undead
+        inc dword ptr [esp] ; undead flag
+        not_undead:
         call dword ptr ds:monster_in_group
+        and dword ptr [esp], eax ; reset flag if not undead
         cmp ebp, GIBBET
         je dragon
         cmp dword ptr [ebx+4], TEMP_ENCH_MARKER
@@ -1779,17 +1819,27 @@ static void __declspec(naked) temp_bane_melee_2(void)
         cmp ecx, SPC_DRAGON_SLAYING
         je dragon
         quit:
+        pop edx
+        mov ecx, esi
+        test edx, edx
+        jle dont_halve
+        shr ecx, 1
+        dont_halve:
         ret
         dragon:
         mov edx, MG_DRAGON
         jmp temp
         undead:
+        test eax, eax
+        jnz quit
         mov edx, MG_UNDEAD
+        add dword ptr [esp], 2
         temp:
-        mov ecx, dword ptr [esp+48]
+        mov ecx, dword ptr [esp+52]
         push eax
         call dword ptr ds:monster_in_group
         pop ecx
+        sub dword ptr [esp], eax
         or eax, ecx
         cmp ebp, GIBBET
         jne quit
@@ -2110,14 +2160,21 @@ static inline void temp_enchants(void)
     patch_byte(0x439983, 0x01); // mov -> add
     hook_call(0x4399bc, temp_elem_damage, 5); // melee weapon(s)
     hook_call(0x48e4b4, temp_swiftness, 6);
+    patch_word(0x48d20e, 0xdf89); // mov edi, ebx
+    hook_call(0x48d210, expire_weapon, 5);
     hook_call(0x48d260, temp_bane_bow_1, 6);
     patch_byte(0x48d28b, 11); // redirect a jump to always reach the below hook
     hook_call(0x48d297, temp_bane_bow_2, 5);
+    patch_word(0x48d2a0, 0xce01); // add esi, ecx
     // melee bane code is repeated for either hand
+    hook_call(0x48ce11, expire_weapon, 7);
     hook_call(0x48ceb6, temp_bane_melee_1, 7);
     hook_call(0x48cecf, temp_bane_melee_2, 5);
+    patch_word(0x48ced8, 0xce01); // add esi, ecx
+    hook_call(0x48cf42, expire_weapon, 7);
     hook_call(0x48cfe1, temp_bane_melee_1, 7);
     hook_call(0x48cffa, temp_bane_melee_2, 5);
+    patch_word(0x48d003, 0xce01); // add esi, ecx
     hook_call(0x41e025, display_temp_enchant, 7);
     patch_dword(0x41dfe5, 4); // one more cycle
     hook_call(0x41de8d, temp_enchant_height, 7);

commit 7af10da60ef6ccc7a86c99a3757a76ad255a2cc3

Overhaul Stealing skill
It's now called Thievery, and is given an additional function - 1%
chance per effective skill level to perform backstab.  Like backstabbing
weapons, this requires your target to face away from you, but a
successful skill-based backstab doubles the total melee damage,
including (almost) all bonuses (unlike weapons which only double their
base damage).  Also, double damage doesn't stack, so a successful
Thievery backstab effectively prevents double damage weapons from
working.  I might also nerf enchanted item bonus to Thievery later.

Also, this commit fixes backstabbing weapons that got broken by 5bc3798.

elemental.c
@@ -410,7 +410,7 @@ enum skills
     SKILL_PERCEPTION = 26,
     SKILL_DISARM_TRAPS = 29,
     SKILL_IDENTIFY_MONSTER = 32,
-    SKILL_STEALING = 34,
+    SKILL_THIEVERY = 34,
     SKILL_ALCHEMY = 35,
     SKILL_LEARNING = 36,
     SKILL_NONE = 37, // used by clubs
@@ -1891,6 +1891,11 @@ static void __declspec(naked) temp_bane_melee_2(void)
 {
     asm
       {
+        test byte ptr [esp+40], 4 ; 1st param, double damage flag
+        jz check_bane
+        xor eax, eax ; double damage doesn`t stack
+        ret
+        check_bane:
         push 0 ; undead flag
         cmp ebp, CORSAIR
         je backstab
@@ -1901,7 +1906,7 @@ static void __declspec(naked) temp_bane_melee_2(void)
         cmp eax, SPC_ASSASSINS
         jne no_backstab
         backstab:
-        test dword ptr [esp+40], 2 ; 1st param, backstab bit
+        test byte ptr [esp+44], 2 ; 1st param, backstab bit
         mov eax, 1 ; return true
         jnz quit
         no_backstab:
@@ -6531,7 +6536,7 @@ static int __stdcall check_skill(int player, int skill, int level, int mastery)
         // skills that are x1/2/3/5 dep. on mastery
         case SKILL_PERCEPTION:
         case SKILL_LEARNING:
-        case SKILL_STEALING:
+        case SKILL_THIEVERY:
             if (current_mastery == GM)
                 current_mastery++;
             current_skill *= current_mastery;
@@ -7478,7 +7483,8 @@ static void __declspec(naked) soul_stealing_weapon(void)
 }
 
 // If the monster can be backstabbed, add 2 to the first parameter
-// of melee damage function.
+// of melee damage function.  If skill-based backstab triggers,
+// add 4 instead.
 static void __declspec(naked) check_backstab(void)
 {
     asm
@@ -7488,6 +7494,33 @@ static void __declspec(naked) check_backstab(void)
         test dh, 6 ; we want no more than +/-512 mod 2048 difference
         jnp quit ; PF == 1 will match 0x000 and 0x110 only
         add dword ptr [esp+4], 2 ; backstab flag
+        push SKILL_THIEVERY
+        call dword ptr ds:get_skill
+        mov edx, eax
+        and edx, SKILL_MASK
+        jz no_thievery
+        mov ecx, edx
+        cmp eax, SKILL_EXPERT
+        jb roll
+        add edx, ecx
+        cmp eax, SKILL_MASTER
+        jb roll
+        add edx, ecx
+        cmp eax, SKILL_GM
+        jb roll
+        lea edx, [edx+ecx*2]
+        roll:
+        push edx
+        call dword ptr ds:random
+        xor edx, edx
+        mov ecx, 100
+        div ecx
+        pop ecx
+        cmp edx, ecx
+        jae no_thievery
+        add dword ptr [esp+4], 2 ; 4 = double total damage flag
+        no_thievery:
+        mov ecx, edi ; restore
         quit:
         mov dword ptr [ebp-8], 4 ; replaced code
         ret
@@ -7501,7 +7534,7 @@ static void __declspec(naked) melee_might_check_chunk(void)
 {
     asm
       {
-        and dword ptr [esp+36], 1
+        test byte ptr [esp+36], 1
       }
 }
 
@@ -10641,6 +10674,22 @@ static void __declspec(naked) perception_extra_item(void)
       }
 }
 
+// Double total melee damage if the flag is set.
+// Currently used by Thievery backstab.
+static void __declspec(naked) double_total_damage(void)
+{
+    asm
+      {
+        add esi, eax ; replaced code, sorta
+        add esi, ebx ; ditto
+        test byte ptr [esp+40], 4
+        jz no_double
+        add esi, esi
+        no_double:
+        ret
+      }
+}
+
 // Tweak various skill effects.
 static inline void skill_changes(void)
 {
@@ -10652,6 +10701,8 @@ static inline void skill_changes(void)
     hook_call(0x426a82, perception_bonus_gold, 5);
     hook_call(0x426c02, perception_extra_item, 11);
     erase_code(0x491276, 12); // remove 100% chance on GM
+    // thievery backstab is checked in check_backstab() above
+    hook_call(0x48d087, double_total_damage, 5);
 }
 
 BOOL WINAPI DllMain(HINSTANCE const instance, DWORD const reason,
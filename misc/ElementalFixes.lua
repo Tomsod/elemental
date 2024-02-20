f3ee6cfcf86c8e0b49870ed9ed7879c0a17a26c0
Tomsod M. <tomsod-m@ya.ru>
2024-02-20 21:59:28 +0300

    Rework the deletion of MMExt tables
    Instead of multiple "deleteme" files, just supply one text file that
    lists all there is to delete, and also delete said file when done.
    
    Also, the old code used a function removed in MMExt v2.3.
    Also also, I now delete re-created .bin files in DataFiles too.

--- misc/ElementalFixes.lua
+++ misc/ElementalFixes.lua
@@ -15,21 +15,21 @@ rawset(Map.SoundSprites, "?ptr", mem.u4[0x4ab49d])
 
 -- The mod overwrites some exe data that is exposed by MMExtension tables.
 -- Since pre-existing tables would take higher precedence than elemental.dll,
--- and outright deleting them is impossible as the mod is distributed
--- via an archive, this simple hack allows overwriting them with dummies
--- that will be deleted by the below code.  Sure, we could also distribute
+-- and deleting them during install is impossible as the mod is distributed
+-- via an archive, we instead place a checklist of files in Data that will be
+-- read and deleted by the below code.  Sure, we could also distribute
 -- actual tables with mod-appropriate data, but that would mean duplicating
 -- the data, which is generally bad.
-for table in path.find("Data/Tables/*.txt") do
-    file = io.open(table)
-    if file then
-        line = file:read()
-        file:close()
-        if line == "deleteme" then
-            path.DeleteFile(table)
-        end
+local name = "Data/elemental.delete.txt"
+local list = io.open(name)
+if list then
+    for line in list:lines() do
+        os.remove(line)
     end
+    list:close()
+    os.remove(name)
 end
+DataTables.LazyMode = true -- prevent the tables from being re-created in v2.3
 
 -- Disable MMExt v2.3 map array extender (we handle it ourselves).
 rawset(Game.MapStats, "count", 78)

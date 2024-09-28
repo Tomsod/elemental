481dd9e22f4c4614d221987fcd88b1ccd3c27283
Tomsod M. <tomsod-m@ya.ru>
2024-09-28 23:53:16 +0300

    Add even more house hirelings
    Picking up from ac921e1, we now have one hireable house NPCs for each
    profession (plus three extra on EI).  I took the names from MM6; most
    of them were male, which saved me the effort of updating mm6text.non.
    
    Also here: fixed healer hire text (it had the old cost), added an unused
    vanilla quest hint, and some minor fixes to other conversation topics.

--- elemental.c
+++ elemental.c
@@ -1369,7 +1369,7 @@ enum monster_buffs
 // new NPC greeting count (starting from 1)
 #define GREET_COUNT 230
 // new NPC topic count
-#define TOPIC_COUNT 628
+#define TOPIC_COUNT 629
 // count of added NPC text entries
 #define NEW_TEXT_COUNT (899-789)
 // new award count

Tomsod/elemental

commit cab17088c43cb8e9696c76981988381a6faf7c61

Randomize Ninja quest cipher a bit
To spice things up for players who already know how to enter Master's
tomb (and thus skip raiding the School of Sorcery), there are now three
more possible button sequences (plus the vanilla one), with one selected
at random for the given playthrough.  Naturally, there are also three
more corresponding cipher and code word pairs to go with these.

I have also changed the sounds the buttons make for the correct code to
still generate the old tone sequence - actually, any five button presses
will now make the same four sounds, except the fifth one is changed to
an unused alternative bell chime if the sequence is incorrect.

Granted, a dedicated cheater can just force-decode the cipher without
the code word using an online tool, but hopefully at least some players
will now be tempted to complete the quest as intended by NWC.

And for those who bother to gather the code word but not to sit down
with a pencil, Bill Lasker now deciphers the message for you, provided
you didn't alienate him earlier (i.e. it requires guild membership).

Finally, the scroll with the code word can now only be looted if the
quest is active (which is important to avoid cluttering the black
market), and the tomb can still be opened and entered on Erathia refill.

elemental.c
@@ -729,7 +729,7 @@ enum items
     ROBE_OF_THE_ARCHMAGISTER = 598,
     RED_APPLE = 630,
     FIRST_ORE = 686,
-    LAST_LOST_ITEM = 702, // scroll of waves
+    LAST_LOST_ITEM = 733, // scroll of waves alt 3
     FIRST_RECIPE = 740,
     LAST_RECIPE = 780,
     MAGIC_EMBER = 785,
@@ -1413,9 +1413,9 @@ enum profession
 };
 
 // New max number of global.evt commands (was 4400 before).
-#define GLOBAL_EVT_LINES 5922
+#define GLOBAL_EVT_LINES 6000
 // New max size of global.evt itself (was 46080 bytes before).
-#define GLOBAL_EVT_SIZE 59068
+#define GLOBAL_EVT_SIZE 60000
 
 #define CURRENT_PLAYER 0x507a6c
 
@@ -1472,9 +1472,9 @@ enum monster_buffs
 // new NPC greeting count (starting from 1)
 #define GREET_COUNT 235
 // new NPC topic count
-#define TOPIC_COUNT 640
+#define TOPIC_COUNT 641
 // count of added NPC text entries
-#define NEW_TEXT_COUNT (917-789)
+#define NEW_TEXT_COUNT (922-789)
 // new award count
 #define AWARD_COUNT 114
 
@@ -10312,7 +10312,7 @@ static int reset_hp_temp, reset_sp_temp;
 static void new_game_data(void)
 {
     memset(&elemdata, 0, sizeof(elemdata));
-    elemdata.version = 403; // v4.0.3
+    elemdata.version = 410; // v4.1.0
     for (int i = 0; i < EXTRA_CHEST_COUNT; i++)
       {
         elemdata.extra_chests[i].picture = i ? 6 : 3; // [0] is bank safe
@@ -10351,10 +10351,54 @@ static void load_game_data(void)
     if (file)
       {
         fread(&elemdata, sizeof(elemdata), 1, file);
-        if (elemdata.version == 400) // save file compatibility
+        if (elemdata.version <= 403) // save file compatibility
           {
-            memset(elemdata.shop_wariness, 0, sizeof(elemdata.shop_wariness));
-            elemdata.version = 402;
+            // The old data structure.
+            enum { OLD_LAST_LOST_ITEM = 702 }; // vanilla scroll of waves
+            struct elemdata_v403
+              {
+                int version;
+                int reputation[12];
+                char artifacts_found[LAST_ARTIFACT-FIRST_ARTIFACT+1];
+                int training[4][SKILL_COUNT];
+                struct beacon bottle;
+                struct map_chest extra_chests[EXTRA_CHEST_COUNT];
+                int difficulty;
+                int last_region;
+                int last_tax_month, last_tax_fame;
+                int current_player;
+                struct item guild_scrolls[32][12];
+                struct item current_orders[42];
+                uint64_t order_timers[42];
+                uint32_t genie;
+                int current_map;
+                uint64_t map_enter_time;
+                struct spell_buff new_pc_buffs[4][NBUFF_COUNT];
+                int last_bank_gold;
+                int quick_spells[4][4];
+                int arena_points;
+                struct beacon beacon_masters[2];
+                int street_npc_seed[MAP_COUNT], street_npc_time[MAP_COUNT];
+                int bard_xp[12], bard_bonus[12];
+                int monster_loot_seed[MAP_COUNT];
+                uint64_t last_rest_time;
+                int next_refill_day[MAP_COUNT];
+                char lost_items[OLD_LAST_LOST_ITEM-FIRST_LOST_ITEM+1];
+                struct item stolen_items[MAX_STOLEN_ITEMS];
+                int shop_wariness[53];
+              } *old_elemdata = (struct elemdata_v403 *) &elemdata;
+            if (elemdata.version < 402) // wariness added in 4.0.2
+                memset(elemdata.shop_wariness, 0,
+                       sizeof(elemdata.shop_wariness));
+            else
+                memmove(elemdata.shop_wariness, old_elemdata->shop_wariness,
+                        sizeof(elemdata.shop_wariness));
+            memmove(elemdata.stolen_items, old_elemdata->stolen_items,
+                    sizeof(elemdata.stolen_items));
+            memset(&elemdata.lost_items[OLD_LAST_LOST_ITEM-FIRST_LOST_ITEM+1],
+                   LOST_NOTRACK, LAST_LOST_ITEM - OLD_LAST_LOST_ITEM);
+            // For 4.0.2 or below we have another fix in load_map_rep().
+            elemdata.version = 410 - (elemdata.version < 403);
           }
       }
     else // probably won't happen; reset all data just in case
@@ -10460,11 +10504,11 @@ static void load_map_rep(void)
           }
       }
     // can't do it on game load, as that hook is before npcs are loaded
-    if (elemdata.version == 402 && !(NPCS[399].bits & NPC_HIRED))
+    if (elemdata.version < 410 && !(NPCS[399].bits & NPC_HIRED))
       {
         // update for a game in progress, unless in party
         NPCS[399].profession = NPCDATA[399].profession;
-        elemdata.version = 403;
+        elemdata.version = 410;
       }
 }
 

commit 0aa917a83b590d328bed1aba6aa5f7e6fb439a9c

Let butler tell you where quest items are
Since we already track quest item location for the black market, let's
add a convenience feature that shares this information with the player,
in case they put an important item in a chest somewhere and forget it.
This is handled by the castle butler, not the judge, because the former
can at least plausibly know where the party has been travelling lately.
Naturally, this is purely informative; unlike vanilla, you won't be
given free copies of quest items, whether they're truly lost or not.

Also here: fix Lasker's Master Dagger tutelage being guild-exclusive
(the script that disables that topic got broken by cab1708 earlier),
and make items left in the proving grounds be properly counted as lost.

elemental.c
@@ -1479,9 +1479,9 @@ enum monster_buffs
 // new NPC greeting count (starting from 1)
 #define GREET_COUNT 235
 // new NPC topic count
-#define TOPIC_COUNT 641
+#define TOPIC_COUNT 642
 // count of added NPC text entries
-#define NEW_TEXT_COUNT (922-789)
+#define NEW_TEXT_COUNT (926-789)
 // new award count
 #define AWARD_COUNT 114
 
@@ -12254,14 +12254,14 @@ static int can_refill_map(int id)
     if (!house) house = get_map_index(MAPSTATS, "mdt15.blv") - 1;
     if (id == dword(CURRENT_MAP_ID) - 1
         || id == castle_id || id == bottle_id // never refilled
-        || !MAPSTATS[id].refill_days // proving grounds/arena
         || !elemdata.next_refill_day[id]) // not visited yet
         return 0; // nothing to refill
     if (id == house && !check_bit(QBITS, QBIT_KILL_TOLBERTI)
                     && !check_bit(QBITS, QBIT_KILL_ROBERT)
+        || !MAPSTATS[id].refill_days // proving grounds/arena
         || MAPSTATS[id].reputation_group == 1
            && check_bit(QBITS, QBIT_LEFT_EMERALD_ISLAND))
-        return -1; // no longer visitable
+        return -1; // no longer visitable or otherwise transient
     return 1; // ok to check
 }
 
@@ -27250,17 +27250,60 @@ static char *arcomage_hint(void)
     return buffer;
 }
 
-// Hook for the above.
+// Let the butler tell you where quest items are, since we already track them.
+static char *i_lost_it(void)
+{
+    static char buffer[300];
+    replace_chest(-1); // for the below call
+    track_lost_items(FALSE);
+    int count = 0;
+    for (int i = 0; i <= LAST_LOST_ITEM - FIRST_LOST_ITEM; i++)
+        if (elemdata.lost_items[i] != LOST_NOTRACK
+            && elemdata.lost_items[i] != LOST_INV)
+            count++;
+    if (!count) return new_npc_text[926-790]; // "haven't lost anything"
+    count = random() % count;
+    int lost, item;
+    for (item = FIRST_LOST_ITEM; item <= LAST_LOST_ITEM; item++)
+      {
+        lost = elemdata.lost_items[item-FIRST_LOST_ITEM];
+        if (lost != LOST_NOTRACK && lost != LOST_INV && !count--)
+            break;
+      }
+    int text = 924 - 790; // truly lost
+    if (lost == dword(CURRENT_MAP_ID)) text--; // left nearby
+    else if (lost != LOST_GONE)
+      {
+        unsigned int day = CURRENT_TIME >> 13;
+        day /= 256 * 60 * 24 >> 13; // avoid long division dependency
+        int check = can_refill_map(lost - 1);
+        if (!check || check > 0 && elemdata.next_refill_day[lost-1] > day)
+            text++; // left in another map
+      }
+    char item_buffer[100];
+    sprintf(item_buffer, COLOR_FORMAT, colors[CLR_ITEM], ITEMS_TXT[item].name);
+    // NB: the second %s is only present in the 'another map' variation
+    sprintf(buffer, new_npc_text[text], item_buffer, MAPSTATS[lost-1].name);
+    return buffer;
+}
+
+// Hook for the above two functions.
 static void __declspec(naked) arcomage_hint_hook(void)
 {
     asm
       {
         cmp ecx, 627 ; the new topic
         je arcomage
+        cmp ecx, 642 ; another one (lost item hint)
+        je lost
         cmp ecx, 200 ; replaced code
         ret
         arcomage:
         call arcomage_hint
+        jmp quit
+        lost:
+        call i_lost_it
+        quit:
         mov dword ptr [CURRENT_TEXT_ADDR], eax
         add dword ptr [esp], 16 ; jump over vanilla code
         ret